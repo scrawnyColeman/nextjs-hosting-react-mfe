0c7083ea3bb899bdae30a74f99a88dae6ae3bba9
scrawnyColeman <thomashutchinson450@gmail.com>
2022-09-25 23:14:29 +0100

    added loading state

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,35 +3,49 @@ import dynamic from "next/dynamic";
 
 const Microfrontend = dynamic(() => import("react-mfe/App"), {
   ssr: false,
-});
-
-const Home: NextPage = () => {
-  return (
+  loading: () => (
     <div
       style={{
-        width: "70%",
-        margin: "auto",
+        width: "100%",
+        height: "250px",
         display: "flex",
-        flexDirection: "column",
-        gap: "1rem",
+        justifyContent: "center",
+        alignItems: "center",
+        border: "1px solid #17111c",
+        boxSizing: "border-box",
+        borderRadius: "20px",
       }}
     >
-      <div
-        style={{
-          width: "100%",
-          height: "250px",
-          borderRadius: "20px",
-          padding: "20px",
-          boxSizing: "border-box",
-          backgroundColor: "#a1d940",
-          color: "#17111c",
-        }}
-      >
-        Local to the host NextJS App
-      </div>
-      <Microfrontend />
+      Loading microfrontend from remote...
     </div>
-  );
-};
+  ),
+});
+
+const Home: NextPage = () => (
+  <div
+    style={{
+      width: "70%",
+      margin: "auto",
+      display: "flex",
+      flexDirection: "column",
+      gap: "1rem",
+    }}
+  >
+    <div
+      style={{
+        width: "100%",
+        height: "250px",
+        borderRadius: "20px",
+        padding: "20px",
+        boxSizing: "border-box",
+        backgroundColor: "#a1d940",
+        color: "#17111c",
+      }}
+    >
+      Local to the host NextJS App
+    </div>
+    <Microfrontend />
+  </div>
+);
 
 export default Home;
